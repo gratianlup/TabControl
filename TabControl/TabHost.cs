7e9ceca35f339de7a6b1dcc70a1671e3ae8b44fb
Gratian Lup <gratianlup@outlook.com>
2013-01-23 14:59:50 +0200

    Update TabControl/TabHost.cs

--- TabControl/TabHost.cs
+++ TabControl/TabHost.cs
@@ -1,14 +1,22 @@
-﻿// Copyright (c) Gratian Lup. All rights reserved.
+// Copyright (c) Gratian Lup. All rights reserved.
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
 //
-//     * Redistributions of source code must retain the above copyright
-//       notice, this list of conditions and the following disclaimer.
-//     * Redistributions in binary form must reproduce the above
-//       copyright notice, this list of conditions and the following
-//       disclaimer in the documentation and/or other materials provided
-//       with the distribution.
+// * Redistributions of source code must retain the above copyright
+// notice, this list of conditions and the following disclaimer.
+//
+// * Redistributions in binary form must reproduce the above
+// copyright notice, this list of conditions and the following
+// disclaimer in the documentation and/or other materials provided
+// with the distribution.
+//
+// * The name "TabControl" must not be used to endorse or promote
+// products derived from this software without prior written permission.
+//
+// * Products derived from this software may not be called "TabControl" nor
+// may "TabControl" appear in their names without prior written
+// permission of the author.
 //
 // THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
 // "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
