1b4d0a871ab82483f690a2199a65bae35a0d9e8f
Gratian Lup <mail@gratianlup.com>
2013-01-27 20:35:03 +0200

    Update copyright notice.

--- TabControl/TabItemCollection.cs
+++ TabControl/TabItemCollection.cs
@@ -1,4 +1,4 @@
-// Copyright (c) Gratian Lup. All rights reserved.
+// Copyright (c) 2007 Gratian Lup. All rights reserved.
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are
 // met:
